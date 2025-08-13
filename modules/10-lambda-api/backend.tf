4ee38e2f683813f4711d34f3091f6a6400a7bd06
zamaaz <maaz.zama15@gmail.com>
2025-08-14 03:19:25 +0530

    Update S3 backend keys for VPC, DocumentDB, RDS, IAM, ALB, Auto Scaling Group, Lambda API, and ECR modules

--- modules/10-lambda-api/backend.tf
+++ modules/10-lambda-api/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "project-backend-ka-naam"
-    key            = "ec2/terraform.tfstate"
+    key            = "lambda/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "users-table"
   }
